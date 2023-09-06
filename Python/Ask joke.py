e3935993e12ed097218b651ad94bdb62679f1d73
Ostap-sudo <143399514+Ostap-sudo@users.noreply.github.com>
2023-09-06 21:15:38 +0200

    Add files via upload

---
+++ Python/Ask joke.py
@@ -0,0 +1,14 @@
+while True:
+    # take input from the user
+    choice = input("Who is the strongest character in Star Wars (Harry_Potter/Gandalf/Obi-Wan/John_Snow): ")
+    # check if choice is one of the four options
+    if choice == 'Harry_Potter':
+        print("You  drink too much butterbeer")
+    elif choice == 'Gandalf':
+        print ("Bilbo Baggins, I'm not trying to rob you, but newermind - it's not Me")
+    elif choice == 'Obi-Wan':
+        print ("Let the Python be with you")
+    elif choice == 'John_Snow':
+        print ("You Know nothing")
+    else:
+        print("Invalid Input")
